93d2eeaaa61f85d007cbbc26220bfd59de8b9290
hanswong <hanswong@ctil.com>
2023-06-14 11:45:21 +0800

    Initial commit

---
+++ StringCipherLib/StringCipher.cs
@@ -0,0 +1,88 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace StringCipherLib
+{
+    public static class StringCipher
+    {
+        // This constant is used to determine the keysize of the encryption algorithm in bits.
+        // We divide this by 8 within the code below to get the equivalent number of bytes.
+        private static int Keysize { get; set; } = 128;
+        public static void SetKeySize(int keySize)
+        {
+            Keysize = keySize;
+        }
+
+        // This constant determines the number of iterations for the password bytes generation function.
+        private static int DerivationIterations { get; set; } = 1000;
+        public static void SetDerivationIterations(int derivationIterations)
+        {
+            DerivationIterations = derivationIterations;
+        }
+
+        private static int Bytesize => Keysize / 8;
+
+        public static string Encrypt(string plainText, string passPhrase)
+        {
+            // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+            // so that the same Salt and IV values can be used when decrypting.  
+            var saltStringBytes = GenerateXBitsOfRandomEntropy(Bytesize);
+            var ivStringBytes = GenerateXBitsOfRandomEntropy(Bytesize);
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            using var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations);
+            var keyBytes = password.GetBytes(Bytesize);
+            using var symmetricKey = Aes.Create();
+            symmetricKey.Mode = CipherMode.CBC;
+            symmetricKey.Padding = PaddingMode.PKCS7;
+            symmetricKey.BlockSize = Keysize;
+            using var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes);
+            using var memoryStream = new MemoryStream();
+            using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
+            var cipherTextBytes = saltStringBytes;
+            cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+            cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+
+        public static string Decrypt(string cipherText, string passPhrase)
+        {
+            // Get the complete stream of bytes that represent:
+            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
+            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+            // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
+            var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Bytesize).ToArray();
+            // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
+            var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Bytesize).Take(Bytesize).ToArray();
+            // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
+            var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Bytesize) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Bytesize) * 2)).ToArray();
+
+            using var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations);
+            var keyBytes = password.GetBytes(Bytesize);
+            using var symmetricKey = Aes.Create();
+            symmetricKey.Mode = CipherMode.CBC;
+            symmetricKey.Padding = PaddingMode.PKCS7;
+            symmetricKey.BlockSize = Keysize;
+            using var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes);
+            using var memoryStream = new MemoryStream(cipherTextBytes);
+            using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            using var streamReader = new StreamReader(cryptoStream, Encoding.UTF8);
+            return streamReader.ReadToEnd();
+        }
+
+        private static byte[] GenerateXBitsOfRandomEntropy(int size)
+        {
+            // 32 Bytes will give us 256 bits.
+            // 16 Bytes will give us 128 bits.
+            var randomBytes = new byte[size]; 
+            using var rngCsp = RandomNumberGenerator.Create();
+            // Fill the array with cryptographically secure random bytes.
+            rngCsp.GetBytes(randomBytes);
+            return randomBytes;
+        }
+    }
+}
